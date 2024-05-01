e99fe669edb58ca81191a5d4016ee24bcb0d85dc
Ebun O <remioludemi@gmail.com>
2024-05-01 14:53:08 -0400

    Fixed backend

--- api/server.js
+++ api/server.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
   next();
 });
 
-const BASE_URL = 'https://oloricatering-api.vercel.app';
+// const BASE_URL = 'https://oloricatering-api.vercel.app';
 
 function sendEmail({ senderName, email, phoneNumber, subject, message }) {
   return new Promise((resolve, reject) => {
@@ -48,7 +48,7 @@ function sendEmail({ senderName, email, phoneNumber, subject, message }) {
   }); 
 }
 
-app.get(`${BASE_URL}/send-email`, (req, res) => {
+app.get("/send-email", (req, res) => {
   sendEmail(req.query)
     .then((response) => res.send(response.message))
     .catch((error) => res.status(500).send(error.message));
