65d44692c9a65943bc9482d3d1e4b818ad28f702
Ebun O <remioludemi@gmail.com>
2024-05-02 21:34:47 -0400

    Testing new cors code

--- api/index.js
+++ api/index.js
@@ -1,64 +1,3 @@
-// require('dotenv').config();
-// const express = require("express");
-// const nodemailer = require("nodemailer");
-// const cors = require("cors");
-// const app = express();
-// const port = 5001;
-
-// app.use(cors());
-// app.use(express.json({ limit: "25mb" }));
-// app.use(express.urlencoded({ limit: "25mb" }));
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
-
-
-// function sendEmail({ senderName, email, phoneNumber, subject, message }) {
-//   return new Promise((resolve, reject) => {
-//     var transporter = nodemailer.createTransport({
-//       service: "gmail",
-//       auth: {
-//         user: process.env.EMAIL,
-//         pass: process.env.EMAIL_PASSWORD,
-//       },
-//     });
-
-//     const mail_configs = {
-//       from: process.env.EMAIL,
-//       to: process.env.EMAIL,
-//       subject: subject,
-//       html: `
-//       <div>
-//         <h4>From: <span><p style="display: inline; font-weight: normal;">${senderName}</p></span></h4>
-//         <h4>Email: <span><p style="display: inline; font-weight: normal;">${email}</p></span></h4>
-//         <h4>Phone Number: <span><p style="display: inline; font-weight: normal;">${phoneNumber}</p></span></h4>
-//         <p>${message}</p>
-//       </div>
-//       `,
-//     };
-//     transporter.sendMail(mail_configs, function (error, info) {
-//       if (error) {
-//         console.log(error);
-//         return reject({ message: `An error has occurred` });
-//       }
-//       return resolve({ message: "Email sent successfully" });
-//     });
-//   }); 
-// }
-
-// app.get("/send-email", (req, res) => {
-//   sendEmail(req.query)
-//     .then((response) => res.send(response.message))
-//     .catch((error) => res.status(500).send(error.message));
-// });
-
-// app.listen(port, () => {
-//   console.log(`nodemailer is listening at http://localhost:${port}`);
-// });
-
 require('dotenv').config();
 const express = require("express");
 const nodemailer = require("nodemailer");
@@ -66,13 +5,30 @@ const cors = require("cors");
 const app = express();
 const port = 5001;
 
-app.use(cors(
-  {
-    origin: ['https://oloricatering.vercel.app'],
-    methods: ["GET", "POST"],
-    credentials: true
-  }
-));
+// app.use(cors(
+//   {
+//     origin: ['https://oloricatering.vercel.app'],
+//     methods: ["GET", "POST"],
+//     credentials: true
+//   }
+// ));
+
+const allowedOrigins = ["https://oloricatering.vercel.app/"];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+  },
+  methods: 'GET',
+  allowedHeaders: '',
+  credentials: false,
+  
+}));
+
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ limit: "25mb" }));
 app.use((req, res, next) => {
