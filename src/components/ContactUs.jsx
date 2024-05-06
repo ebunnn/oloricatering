274cb20d25022d16c5c50167011822e61bd3353c
Ebun O <remioludemi@gmail.com>
2024-05-06 19:39:36 -0400

    Fixed api link

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -21,7 +21,7 @@ export default function ContactUs() {
           }
         axios.defaults.withCredentials = true;
         axios
-          .get("https://oloricatering-api.vercel.app/send-email", { //http://localhost:5001/send-email https://oloricatering-api.vercel.app/send-email"
+          .get("https://oloricatering-api.vercel.app", { //http://localhost:5001/send-email https://oloricatering-api.vercel.app"
             params: {
               senderName,
               email,
