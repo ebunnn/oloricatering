c418ad31b55c420386cc15c77328ac071bda44cd
Ebun O <remioludemi@gmail.com>
2024-05-06 11:38:02 -0400

    Removed Access-Control-Allow-Origin code

--- api/index.js
+++ api/index.js
@@ -16,12 +16,12 @@ app.use(cors(
 
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ limit: "25mb" }));
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
 
 
 async function sendEmail({ senderName, email, phoneNumber, subject, message }) {
