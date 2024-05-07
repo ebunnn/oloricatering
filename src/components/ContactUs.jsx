36640a26f5b3c1b63f7d0696260c712376a68ede
Ebun O <remioludemi@gmail.com>
2024-05-07 13:05:14 -0400

    Changed email

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -21,7 +21,7 @@ export default function ContactUs() {
           }
         axios.defaults.withCredentials = true;
         axios
-          .get("https://oloricatering-api.vercel.app", { //http://localhost:5001/send-email https://oloricatering-api.vercel.app"
+          .get("https://oloricatering-api.vercel.app", { //http://localhost:5001 https://oloricatering-api.vercel.app"
             params: {
               senderName,
               email,
@@ -55,7 +55,7 @@ export default function ContactUs() {
             <h2>Contact Information</h2>
             <p>Contact us to make an order, ask any questions, or receive more information.</p>
             <p><FontAwesomeIcon icon={faPhone} />  <a href="tel:+14436758105">+1{"("}443{")"}-675-8105</a></p>
-            <p><FontAwesomeIcon icon={faMailBulk} /> <a href="mailto:ebunoludemi@gmail.com">oloricatering@gmail.com</a></p>
+            <p><FontAwesomeIcon icon={faMailBulk} /> <a href="mailto:oloricateringservices@gmail.com">oloricateringservices@gmail.com</a></p>
         </div>
 
 
